cb531a4adcdc0e4dac7e48e84360a1d77c275a57
Y Trinh <trinhnhay@gmail.com>
2023-11-03 14:21:27 -0500

    Transaction update + Credit card created

--- src/main/java/com/example/demo/CreditCard/CreditCardRepository.java
+++ src/main/java/com/example/demo/CreditCard/CreditCardRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CreditCardRepository extends JpaRepository <CreditCard, Integer> {
+public interface CreditCardRepository extends JpaRepository <CreditCard, String> {
+
+
 }
--- src/main/java/com/example/demo/CreditCard/CreditCardService.java
+++ src/main/java/com/example/demo/CreditCard/CreditCardService.java
@@ -1,25 +1,56 @@
 package com.example.demo.CreditCard;
+import com.example.demo.Customer.CustomerRepository;
 import com.example.demo.Exception.RequestException;
-import com.example.demo.Transaction.TransactionService;
+import com.example.demo.Transaction.Transaction;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreditCardService {
     private final CreditCardRepository creditCardRepository;
-    private final TransactionService transactionService;
+    private final CustomerRepository customerRepository;
     @Autowired
-    public CreditCardService (CreditCardRepository creditCardRepository, TransactionService transactionService ){
+    public CreditCardService (CreditCardRepository creditCardRepository, CustomerRepository customerRepository){
         this.creditCardRepository = creditCardRepository;
-        this.transactionService = transactionService;
+        this.customerRepository= customerRepository;
     }
 
     public void addCreditCard (CreditCard creditCard, String username) {
-        if (transactionService.isUsernamePresent(username)) {
+        if (customerRepository.existsById(username)) {
             creditCard.setUsername(username);
+            if (creditCard.getCurrentBalance()==null)
+                creditCard.setCurrentBalance(0.0);
             creditCardRepository.save(creditCard);
         }
         else
             throw new RequestException("This username does not exist");
+    }
+
+    public CreditCard doesCreditCardExist(String cardNumber){
+        return creditCardRepository.findById(cardNumber)
+                .orElseThrow(()-> new RequestException(
+                        "This credit card does not exist"
+                ));
+    }
+
+    public Double getCurrentBalance(String cardNumber){
+        CreditCard creditCard = doesCreditCardExist(cardNumber);
+        return creditCard.getCurrentBalance();
+    }
+
+    @Transactional
+    public void updateBalance (Transaction transaction){
+
+        CreditCard  creditCard = doesCreditCardExist(transaction.getCardNumber());
+
+        if (transaction.getTransCategory().equals("payment"))
+            creditCard.setPaymentDue(transaction.getTransAmount());
+
+        Double currentBalance = creditCard.getCurrentBalance() + transaction.getTransAmount();
+        Double availableCredit = creditCard.getCreditLimit() - currentBalance ;
+        creditCard.setAvailableCredit(availableCredit);
+
+        creditCard.setCurrentBalance(currentBalance);
     }
 }
